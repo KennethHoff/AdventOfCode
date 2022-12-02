ff35261ba96f5a320e9b4325f5ba46f33740d32d
Kenneth Hoff <kenneth.98@hotmail.com>
2022-12-02 19:23:04 +0100

    Move everything so far into Day 1
    I thought Day 1 Part 2 was Day 2 :shrug:

--- 2022/csharp/src/AdventOfCode.Day1/Program.cs
+++ 2022/csharp/src/AdventOfCode.Day1/Program.cs
@@ -1,7 +1,14 @@
 using AdventOfCode.Shared;
 
-Console.WriteLine(new FileParser("data.nlsv")
+Console.WriteLine("Food for the #1 elf: " + new FileParser("data.nlsv")
 	.ParseElves()
 	.Select(x => x.TotalCalories)
 	.OrderDescending()
 	.FirstOrDefault());
+
+Console.WriteLine("Food for the top 3 elves: " + new FileParser("data.nlsv")
+	.ParseElves()
+	.Select(x => x.TotalCalories)
+	.OrderDescending()
+	.Take(3)
+	.Sum(x => x));
--- 2022/csharp/src/AdventOfCode.Day2/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using AdventOfCode.Shared;
-
-Console.WriteLine(new FileParser("data.nlsv")
-	.ParseElves()
-	.Select(x => x.TotalCalories)
-	.OrderDescending()
-	.Take(3)
-	.Sum(x => x));
