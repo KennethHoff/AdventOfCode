d9294024071fec63b72ab48818f94e7b604d19e9
Kenneth Hoff <kenneth.98@hotmail.com>
2022-12-04 13:09:30 +0100

    Rename parameter for improved clarity

--- 2022/csharp/src/AdventOfCode.Day4/Models/Assignment.cs
+++ 2022/csharp/src/AdventOfCode.Day4/Models/Assignment.cs
@@ -2,11 +2,11 @@ namespace AdventOfCode.Day4.Models;
 
 internal readonly record struct Assignment(Range Range)
 {
-	public bool IsFullyEncapsulatedBy(Assignment range)
-		=> range.Range.Start.Value <= Range.Start.Value && range.Range.End.Value >= Range.End.Value;
+	public bool IsFullyEncapsulatedBy(Assignment other)
+		=> other.Range.Start.Value <= Range.Start.Value && other.Range.End.Value >= Range.End.Value;
 
-	public bool IsPartiallyEncapsulatedBy(Assignment range)
-		=> (range.Range.Start.Value <= Range.Start.Value && range.Range.End.Value >= Range.Start.Value) ||
-		   (range.Range.Start.Value <= Range.End.Value && range.Range.End.Value >= Range.End.Value);
+	public bool IsPartiallyEncapsulatedBy(Assignment other)
+		=> (other.Range.Start.Value <= Range.Start.Value && other.Range.End.Value >= Range.Start.Value) ||
+		   (other.Range.Start.Value <= Range.End.Value && other.Range.End.Value >= Range.End.Value);
 }
 
