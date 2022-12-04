6c1a804f8c38f506e17b1416cc3500293006e804
Kenneth Hoff <kenneth.98@hotmail.com>
2022-12-04 13:22:57 +0100

    Complete Day 4 (#3)

---
+++ 2022/csharp/src/AdventOfCode.Day4/Models/AssignmentPair.cs
@@ -0,0 +1,9 @@
+namespace AdventOfCode.Day4.Models;
+
+internal sealed record class AssignmentPair(Assignment Assignment1, Assignment Assignment2)
+{
+	public bool IsFullyOverlapping => Assignment1.IsFullyEncapsulatedBy(Assignment2) || Assignment2.IsFullyEncapsulatedBy(Assignment1);
+
+	public bool IsPartiallyOverlapping => Assignment1.IsPartiallyEncapsulatedBy(Assignment2) || Assignment2.IsPartiallyEncapsulatedBy(Assignment1);
+}
+
---
+++ 2022/csharp/src/AdventOfCode.Day4/Program.cs
@@ -0,0 +1,9 @@
+﻿using AdventOfCode.Day4.Logic;
+
+Console.WriteLine("Number of elves whose jobs are completely useless: " + new OverlappingAssignmentAnalyzer("data.csv")
+	.GetAssignments()
+	.Count(x => x.IsFullyOverlapping));
+
+Console.WriteLine("Number of elves whose jobs are partially useless: " + new OverlappingAssignmentAnalyzer("data.csv")
+	.GetAssignments()
+	.Count(x => x.IsPartiallyOverlapping));
