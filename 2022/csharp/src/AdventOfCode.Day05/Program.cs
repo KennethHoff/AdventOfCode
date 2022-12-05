f1c05de17eb3ce31eb2d81a517998c6e21a2dff1
Kenneth Hoff <kenneth.98@hotmail.com>
2022-12-05 22:26:54 +0100

    Rename file

--- 2022/csharp/src/AdventOfCode.Day05/Program.cs
+++ 2022/csharp/src/AdventOfCode.Day05/Program.cs
@@ -3,12 +3,12 @@ using AdventOfCode.Day05.Models;
 
 Console.WriteLine("Day 5");
 
-Console.WriteLine("Inefficient stacking: " + new CraneOperator<InefficientCrateStacker>("data.skoff")
+Console.WriteLine("Inefficient stacking: " + new CraneOperator<InefficientCrateStacker>("data.scuff")
 	.SortCrateStacks()
 	.Select(x => x.Value.FirstOrDefault().Value.ToString())
 	.Aggregate((x, y) => x + y));
 
-Console.WriteLine("Efficient stacking: " + new CraneOperator<EfficientCrateStacker>("data.skoff")
+Console.WriteLine("Efficient stacking: " + new CraneOperator<EfficientCrateStacker>("data.scuff")
 	.SortCrateStacks()
 	.Select(x => x.Value.FirstOrDefault().Value.ToString())
 	.Aggregate((x, y) => x + y));
