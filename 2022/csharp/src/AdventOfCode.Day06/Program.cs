6cdda97742c780cc5413fb808fc33aec3778a239
Kenneth Hoff <kenneth.98@hotmail.com>
2022-12-06 09:21:27 +0100

    Complete Day 6 (#6)
    * Complete Day 6 Part 1
    
    * Complete Day 6 Part 2
    
    * Cleanup
    
    * Remove folder

--- 2022/csharp/src/AdventOfCode.Day06/Program.cs
+++ 2022/csharp/src/AdventOfCode.Day06/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Day 6");
+﻿using AdventOfCode.Day06.Logic;
+
+Console.WriteLine("Day 6");
+
+Console.WriteLine("End of message marker count: " + new EndOfMessageCommunicationSystemDecoder("data.gg")
+	.GetMessages()
+	.First()
+	.Length);
+
+Console.WriteLine("Start of message marker count: " + new StartOfMessageCommunicationSystemDecoder("data.gg")
+	.GetMessages()
+	.First()
+	.Length);
